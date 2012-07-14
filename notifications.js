9117a8b5050cdd178a6ca30f35493e10cd20f68d
Max Winde <max.winde@gmail.com>
2012-07-14 21:28:55 +0200

    enhanced notifications framework

--- notifications.js
+++ notifications.js
@@ -1,12 +1,18 @@
 var _ = require('underscore');
 
 module.exports.init = function(io) {
+    var allCategories = ['tabs', 'clients', 'browsers'];
+
+    var roomName = function(username, category) {
+        return username + '/' + category;
+    }
+
     io.on('connection', function(socket) {
         socket.on('login', function(data, next) {
             Browser.Model.authenticatedBrowserOrClient(data.username, data.password, function(err, browser) {
                 if (browser) {
                     _.each(data.categories, function(category) {
-                        socket.join(data.username + '/' + category);
+                        socket.join(roomName(data.username, category));
                     });
                     next({success: true});
                 } else {
@@ -14,41 +20,66 @@ module.exports.init = function(io) {
                 }
             });
         });
-    })
+
+        socket.on('logout', function(data, next) {
+            Browser.Model.authenticatedBrowserOrClient(data.username, data.password, function(err, browser) {
+                if (browser) {
+                    _.each(allCategories, function(category) {
+                        socket.leave(roomName(data.username, category));
+                    });
+                    next({success: true});
+                } else {
+                    next({success: false});
+                }
+            });
+        })
+    });
 
     return {
-        notify: function(browserId, category, event, data) {
-            io.sockets.in(browserId + '/' + category).emit(event, data);
+        notify: function(browser, category, event, data) {
+            var ids = [ browser.uniquename ];
+
+            _.each(browser.clients, function(client) {
+                if (client.claimed) {
+                    ids.push(client.uniquename);
+                }
+            });
+
+            _.each(ids, function(id) {
+                var sendData = data;
+                sendData.identifier = id;
+                io.sockets.in(roomName(id, category)).emit(event, sendData);
+            })
         },
 
         tabsReplaced: function(browser) {
             var tabs = _.map(browser.tabs, function(tab) {
                 return tab.jsonObject();
             });
-            this.notify(browser.uniquename, 'tabs', 'tabsReplaced', { tabs: tabs });
+            this.notify(browser, 'tabs', 'tabsReplaced', { tabs: tabs });
         },
 
         tabsUpdated: function(browser, tabs) {
             tabs = _.map(browser.tabs, function(tab) {
                 return tab.jsonObject();
             });
-            this.notify(browser.uniquename, 'tabs', 'tabsUpdated', { tabs: tabs });
+            this.notify(browser, 'tabs', 'tabsUpdated', { tabs: tabs });
         },
 
         claimClient: function(browser, client) {
-            this.notify(browser.uniquename, 'clients', 'claimClient', { client: client.jsonObject() });
+            this.notify(browser, 'clients', 'claimClient', { client: client.jsonObject() });
         },
 
         clientSeen: function(browser, client) {
-            this.notify(browser.uniquename, 'clients', 'clientSeen', { client: client.jsonObject() });
+            this.notify(browser, 'clients', 'clientSeen', { client: client.jsonObject() });
         },
 
         clientRemoved: function(browser, client) {
-            this.notify(browser.uniquename, 'clients', 'clientRemoved', { client: client.jsonObject() });
+            this.notify(browser, 'clients', 'clientRemoved', { client: client.jsonObject() });
         },
 
         browserUpdated: function(browser) {
-            this.notify(browser.uniquename, 'browsers', 'browserUpdated', { browser: browser.jsonObject() });
+            this.notify(browser, 'browsers', 'browserUpdated', { browser: browser.jsonObject() });
         }
     }
 }
